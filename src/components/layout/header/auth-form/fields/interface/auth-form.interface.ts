2738add8b3c6b254aee360e5d164eea432b506ff
FromZeroTo1 <mark.makarov98@mail.ru>
2023-12-20 15:12:48 +0400

    first commit

--- src/components/layout/header/auth-form/fields/interface/auth-form.interface.ts
+++ src/components/layout/header/auth-form/fields/interface/auth-form.interface.ts
@@ -1,7 +1,7 @@
-import { AuthType } from '@/shared/types/auth/auth.type'
+import { TypeAuth } from '@/shared/types/auth/auth.type'
 import { Dispatch, SetStateAction } from 'react'
 
 export interface IAuthForm {
-	type: AuthType
-	setType: Dispatch<SetStateAction<AuthType>>
+	type: TypeAuth
+	setType: Dispatch<SetStateAction<TypeAuth>>
 }
