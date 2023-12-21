e273ed8838ffe8e7907250974dbab782ae9fb82e
FromZeroTo1 <mark.makarov98@mail.ru>
2023-12-21 14:06:27 +0400

    first commit

--- next.config.js
+++ next.config.js
@@ -23,12 +23,12 @@ const nextConfig = {
 		return [
 			{
 				source: '/api/:path*',
-				destination: 'https://orange-red-hatchling-hose.cyclic.app/api/:path*',
+				destination: 'https://amber-viper-tie.cyclic.app/api/:path*',
 			},
 			{
 				source: '/uploads/:path*',
 				destination:
-					'https://orange-red-hatchling-hose.cyclic.app/uploads/:path*',
+					'https://amber-viper-tie.cyclic.app/uploads/:path*',
 			},
 		]
 	},
