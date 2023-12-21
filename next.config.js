ddd38cb7c5ab542955d128f6ecc2e6e187c38b49
FromZeroTo1 <mark.makarov98@mail.ru>
2023-12-21 19:53:27 +0400

    first commit

--- next.config.js
+++ next.config.js
@@ -23,11 +23,11 @@ const nextConfig = {
 		return [
 			{
 				source: '/api/:path*',
-				destination: 'http://localhost:4200/api/:path*',
+				destination: 'https://sore-lime-codfish-belt.cyclic.app/api/:path*',
 			},
 			{
 				source: '/uploads/:path*',
-				destination: 'http://localhost:4200/uploads/:path*',
+				destination: 'https://sore-lime-codfish-belt.cyclic.app/uploads/:path*',
 			},
 		]
 	},
